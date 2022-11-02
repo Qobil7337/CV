230353aeb660c1693835212ee87e54b220a0e7bf
qobilashurov <113908288+qobilashurov@users.noreply.github.com>
2022-11-02 21:06:30 +0500

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,23 @@
+
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1*6;
+randomNumber1 = Math.floor(randomNumber1)+1;
+randomNumber1 = "images/dice" + randomNumber1 +".png";
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2*6;
+randomNumber2 = Math.floor(randomNumber2)+1;
+randomNumber2 = "images/dice" + randomNumber2 +".png";
+
+document.querySelector(".img1").setAttribute("src" , randomNumber1);
+document.querySelector(".img2").setAttribute("src" , randomNumber2);
+
+if (randomNumber1>randomNumber2) {
+  document.getElementById("h1").innerHTML = "Player 1 Wins";
+}
+else if (randomNumber1<randomNumber2) {
+   document.getElementById("h1").innerHTML = "Player 2 Wins";
+}
+else {
+   document.getElementById("h1").innerHTML = "Draw";
+}
